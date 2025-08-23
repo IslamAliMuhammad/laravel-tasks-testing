21cdad96afa8fbaa155b5804e85e0a76e24fa925
Islam Ali <islam.ali.muhammad@gmail.com>
2025-08-23 16:34:17 +0300

    feat(api): Login and Register implementations

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\API\AuthController;
 
 
-Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store']);
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index']);
+    Route::post('/tasks', [App\Http\Controllers\TaskController::class, 'store']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
